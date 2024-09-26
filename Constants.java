bdf06260ead69c3dab36c474dc33662d4a3174cd
GulshanAgrwal <agrawalgulshan9@gmail.com>
2024-09-26 15:17:42 +0530

    Add files via upload

---
+++ Constants.java
@@ -0,0 +1,31 @@
+/***
+ * description : conversion of all bases into another base and addition, substraction, multiplicatio, and division.
+ * Name : Gulshan Agrawal
+ * Date : 26-09-2024
+ */
+
+import javax.management.StringValueExp;
+
+public class Constants {
+    public static final String CHOOSE = "Select an operation : ";
+    public static final String OPTION1 =  "Enter 1 to convert Decimal to Other";
+    public static final String OPTION2 = "Enter 2 to convert Other to Decimal";
+    public static final String OPTION3 = "Enter 3 to convert Other to Other";
+    public static final String OPTION4 = "Enter 4 to perform addition";
+    public static final String OPTION5 = "Enter 5 to perform substraction";
+    public static final String OPTION6 = "Enter 6 to perform multiplication";
+    public static final String OPTION7 = "Enter 7 to perform division";
+    public static final String OPTION9 = "Enter 9 to exit";
+    public static final String ENTERNUMBER1 = "Enter the number to be converted : ";
+    public static final String ENTERBASE1 = "Enter the desired base : ";
+    public static final String VALIDBASE = "Please enter a valid base";
+    public static final String VALIDINPUT = "Please enter a valid input";
+    public static final String CURRENTBASE = "Enter base of the entered number";
+    public static final String FIRSTNUMBER = "Enter the first number : ";
+    public static final String SECONDNUMBER = "Enter the second number : ";
+    public static final String THANKS = "THANKS";
+    public static final String DIFFERENT = "Both the bases are different";
+    public static final String TRYAGAIN = "Please try again";
+    public static final String INT = "Please enter such values whose answer should not exceed INT";
+    public static final String DIV = "Please enter such values whose answer is also INT";
+}
